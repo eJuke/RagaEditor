3e80ee614e20b7706131c91d1c8cddbbb8573688
BeInLife <evgprogger@gmail.com>
2014-05-06 22:56:29 +0400

    Komment

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    cout << "Hello World";
+    cout << "Hello World"; //kto-to huy
     return a.exec();
 }
